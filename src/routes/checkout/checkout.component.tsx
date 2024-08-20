8010578044066a17ba24f865bfc80097c3635679
Yen Yen <yenyen_choo@singaporeair.com.sg>
2024-08-20 15:58:58 +0800

    pay by krisplus button and open QR code window

--- src/routes/checkout/checkout.component.tsx
+++ src/routes/checkout/checkout.component.tsx
@@ -3,14 +3,47 @@ import { AppState } from "../../state";
 import "./checkout.styles.scss";
 import { useSelector } from "react-redux";
 import Button from "../../components/button/button.component";
+import QRCode from "qrcode";
 
 export default function Checkout() {
   const cartItems = useSelector((state: AppState) => state.cartState.cartItems);
-  const cartTotal = cartItems.reduce((total, cartItem: any) => total + cartItem.quantity * cartItem.price, 0)
+  const cartTotal = cartItems.reduce(
+    (total, cartItem: any) => total + cartItem.quantity * cartItem.price,
+    0
+  );
 
-  const payByKrisplus = () => {
+  const payByKrisplus = async () => {
+    const url =
+      "krispay://payment_deeplink?pos_id=t1&callback=https%3A%2F%2Fwww.myonlinestore.com%2Fkp-payment-callback&payment_amt=34.99&currency=AUD&qr_timestamp=1722326071&payment_ref=txn3216352";
+    try {
+      // Generate the QR code as a data URL
+      const qrCodeDataUrl = await QRCode.toDataURL(url);
 
-  }
+      // Open a new window and write the QR code into it
+      const paymentWindow = window.open(
+        "",
+        "pay by Krisplus window",
+        "width=500,height=500"
+      );
+      if (paymentWindow) {
+        paymentWindow.document.write(`
+          <html>
+            <head>
+              <title>Pay by Krisplus</title>
+            </head>
+            <body>
+              <div id="qrCode" style="display: flex; align-items: center; justify-content: center; height: 100vh;">
+                <img src="${qrCodeDataUrl}" alt="QR Code" />
+              </div>
+            </body>
+          </html>
+        `);
+        paymentWindow.document.close();
+      }
+    } catch (error) {
+      console.error("Failed to generate QR code", error);
+    }
+  };
 
   return (
     <>
